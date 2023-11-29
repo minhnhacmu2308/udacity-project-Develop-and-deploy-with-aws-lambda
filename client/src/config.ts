e6dc94cc42bb96c709b3c1055542f9b8acf6282b
Your Name <you@example.com>
2023-11-29 15:58:24 +0700

    final

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'm0fnuyogz8'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-x6b7jt23y0xtca0z.us.auth0.com',    // Domain from Auth0
+  clientId: 'mYBRHSg9j97h2h1EmqqT9cGmGJtEPaDR',  // Client id from an Auth0 application
+  callbackUrl: 'http://localhost:3000/callback'
+}
